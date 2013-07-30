0ba6a3bb22e826d49ceef52eaf41263ae9b769c0
Ronald Kaiser <raios.catodicos@gmail.com>
2013-07-30 19:04:09 -0300

    minor changes in balls.py

--- balls/balls.py
+++ balls/balls.py
@@ -12,8 +12,8 @@ while running:
     screen.fill((0, 0, 0))
     l = Lumino('/dev/ttyUSB0') 
     ra, rb = l.get()
-    pygame.draw.circle(screen, (255, 0, 0), (300, 300), ra/2, 0)
-    pygame.draw.circle(screen, (0, 255, 0), (700, 300), rb/2, 0)
+    pygame.draw.circle(screen, (255, 0, 0), (300, 300), ra/3, 0)
+    pygame.draw.circle(screen, (0, 255, 0), (700, 300), rb/3, 0)
     pygame.display.update()
 
     for event in pygame.event.get():
